1892b2235a69fae157ac30aa7e1cd24010e704cc
Unknown <kgrager@mail.csuchico.edu>
2017-04-26 17:12:35 -0700

    Page randomly prints a Chico location without repeating.

--- gallery.js
+++ gallery.js
@@ -1,6 +1,18 @@
-
+var chico = [
+	"Sierra Nevada Brewery",
+	"that old covered bridge",
+	"Roots brunch",
+	"Chico State",
+	"Maltese"
+];
+var current = 0;
 
 function shuffleGallery()
 {
-
+	var index = 0;
+	do {
+		index = Math.floor(Math.random() * (chico.length-1));
+	} while (index == current);
+	console.log(chico[index]);
+	current = index;
 }
